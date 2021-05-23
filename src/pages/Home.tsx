a852d4cc0fe3e77fe24c3b9257c18eb425465a2c
Comdinheiro Contato <andre.bovolini@comdinheiro.com.br>
2021-05-23 09:22:59 -0300

    finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,46 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (newTaskTitle !== '') {
+      let newTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+
+      setTasks(oldArray => [...oldArray, newTask])
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    let tasksArray = [...tasks];
+
+    tasksArray = tasksArray.map((task, i) => {
+      if (task.id === id) {
+        return {
+          id: task.id,
+          title: task.title,
+          done: true
+        }
+      } else {
+        return task
+      }
+    })
+
+    setTasks([...tasksArray])
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    let tasksArray = [...tasks];
+
+    let newArray = tasksArray.filter(
+      task => task.id !== id
+    )
+
+    setTasks([...newArray])
   }
 
   return (
